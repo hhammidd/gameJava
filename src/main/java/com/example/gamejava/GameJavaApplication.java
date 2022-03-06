03fdf666e8f32528b9487d5a533fdd1332de5f46
hamid <hamid.shafikani@gmail.com>
2022-03-06 12:15:38 +0100

    Initial

---
+++ src/main/java/com/example/gamejava/GameJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.gamejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gamejava/GameJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gamejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
